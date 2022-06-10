6b16e6b2403c49e5687d034cd50e5ec140ae494c
hungnmph07114 <itisloves2@gmail.com>
2022-06-10 11:07:05 +0700

    hung

--- tiktoc/src/hook/Content.js
+++ tiktoc/src/hook/Content.js
@@ -1,8 +1,19 @@
-import React,{useState } from "react";
+import React,{useState,useEffect } from "react";
+//useEffect (callback)
+// - Goik callbacl mỗi khi component re -render
+//useEffect (callback,[])
+//useEffect (callback,[Dj])
+//----------
+//1. Callback luôn được gọi sau khi component mounted
 function Content(){
+    const [title,setTitle] = useState('')
+   useEffect(() =>{
+       console.log('mounted')
+   })
     return(
         <div>
-       <h1>chao</h1>
+      <input value={title}
+      onChange={event => setTitle(event.target.value)}/>
         </div>
     )
 }
